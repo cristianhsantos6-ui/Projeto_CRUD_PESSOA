580725a61767080a2562e5784189733900dea323
cristian <cristian.h.santos6@aluno.senai.br>
2026-06-22 15:01:33 -0300

    adicionar

---
+++ js/script_pessoas.js
@@ -0,0 +1,46 @@
+//PEGANDO ELEMENTOS DO DOM
+const formPessoa = document.querySelector('#form-pessoa')
+const divLista = document.querySelector('#div-lista-pessoas')
+
+
+//CRIANDO ARRAY pessoas
+const pessoas = []
+
+//CAPTURAR O EVENTO submit DO FORMULÁRIO
+formPessoa.addEventListener('submit', (evt) => {
+    //INTERROMPER O EFEITO PADRÃO DE SUBMETER OS DADOS DO FORMULÁRIO
+    evt.preventDefault()
+
+    //CRIAR UM OBJETO FORMULÁRIO
+    const dadosFormPessoa = new FormData(formPessoa)
+
+    //CRIAR UM OBJETO LITERAL
+    const pessoa = {
+        nome: dadosFormPessoa.get('nome'),
+       idade: dadosFormPessoa.get('idade'),
+       renda: dadosFormPessoa.get('renda')
+    }
+
+    //CHAMANDO A FUNÇÃO addPessoa E A PASSANDO O OBJETO LITERAL pessoa
+    addPessoa(pessoa)
+
+})
+
+//CRIANDO A FUNÇÃO ADICIONAR PESSOA 
+const addPessoa = (objPessoa) => {
+    //ADICIONANDO O OBJETO LITERAL NO ARRAY PESSOAS
+    pessoas.push(objPessoa)
+
+    //APÓS ADICIONAR O OBJETO LITERAL NO ARRAY CHAMA A FUNÇÃO listPessoas
+    listPessoas
+    listPessoas()
+}
+
+//FUNÇÃO PARA LISTAR PESSOAS DO ARRAY
+const listPessoas = () => {
+    //PERCORRER O ARRAY pessoas COM O foreach
+    pessoas.forEach((elem, i) => {
+        divLista.innerHTML += `${i} - ${elem.nome} - ${elem.idade}, ${elem.renda} <br>`
+    })
+}
+
